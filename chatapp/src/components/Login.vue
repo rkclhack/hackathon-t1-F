d68c820843f2d7c103b2d365a389844aec34a386
togawakeita <togakei.27.nagano@gmail.com>
2025-07-30 16:08:36 +0900

    fix

--- chatapp/src/components/Login.vue
+++ chatapp/src/components/Login.vue
@@ -22,30 +22,17 @@ const onEnter = () => {
   // ユーザー名が入力されているかチェック
   if (!inputUserName.value.trim()) {
     alert('入力が空です。値を入力してください。')
+    return
   }
+
   // 入室メッセージを送信
+  socket.emit("enterEvent", inputUserName.value)
 
   // 全体で使用するnameに入力されたユーザー名を格納
+  userName.value = inputUserName.value
 
   // チャット画面へ遷移
   router.push({ name: "chat" })
-// #region global state
-const userName = inject("userName")
-// #endregion
-
-// #region local variable
-const router = useRouter()
-const socket = socketManager.getInstance()
-// #endregion
-
-// #region reactive variable
-const inputUserName = ref("")
-// #endregion
-
-// #region browser event handler
-// 入室メッセージをクライアントに送信する
-const onEnter = () => {
-  // ユー
 }
 // #endregion
 </script>
